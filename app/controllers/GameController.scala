b5b15c453c84f097da84fd702260ea061a3a81ed
Eugenia Simich <eugeniasimich@gmail.com>
2020-09-04 02:34:14 -0300

    ADD post endpoint that parses a Game object

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsString, Json, JsError}
 import model.GameModel._
 
 @Singleton
@@ -11,4 +11,18 @@ class GameController @Inject()(cc: ControllerComponents) extends AbstractControl
   def newGame(x: Int, y: Int, n: Int) = Action {
     Ok(Json.toJson(a))
   }
+
+  def openCell() = Action(parse.json) { request =>
+    val placeResult = request.body.validate[Game]
+    placeResult.fold(
+      errors => {
+        BadRequest(Json.obj("message" -> JsError.toJson(errors)))
+      },
+      g => {
+        println(g.data.mkString(","))
+        Ok(Json.obj("message" -> ("Ok")))
+      }
+    )
+  }
+
 }
